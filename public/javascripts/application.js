8552ee2faaadc7791e79eeb0a6def27e0378a10c
Pedro Coelho <pedro.luis.coelho@gmail.com>
2010-10-20 01:05:07 +0100

    Now uses jQuery! Added the basis for i18n.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -4,20 +4,15 @@
 // I owe a litre of beer to @rbates for the following 2 functions
 // -- Stripteasus Explosivus
 
-function insert_fields(link, method, content) {
-  var new_id = new Date().getTime();
-  var regexp = new RegExp("new_" + method, "g")
-  $(link).up().insert({
-    before: content.replace(regexp, new_id)
-  });
+function remove_fields(link) {
+  $(link).prev("input[type=hidden]").val("1");
+  $(link).closest(".fields").hide();
 }
 
-function remove_fields(link) {
-  var hidden_field = $(link).previous("input[type=hidden]");
-  if (hidden_field) {
-    hidden_field.value = '1';
-  }
-  $(link).up(".fields").hide();
+function insert_fields(link, association, content) {
+  var new_id = new Date().getTime();
+  var regexp = new RegExp("new_" + association, "g")
+  $(link).parent().before(content.replace(regexp, new_id));
 }
 
 function add_weird_character(elementid, character){
